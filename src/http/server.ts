8613cdd587b7729133a77085826417ea41166ff0
David Kukert <dkukert@kukert.com>
2025-08-12 09:34:24 -0300

    fix: add o modulo de mangas a instancia principal do Elysia na API.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import Elysia from 'elysia'
+import mangas from '@/modules/mangas'
 import users from '@/modules/users'
 
 const app = new Elysia({ aot: true, normalize: 'typebox' })
 	.use(users)
+	.use(mangas)
 	.listen({ port: 3333, hostname: '0.0.0.0' })
 
 console.log(`DK Mangas API is running at ${app.server?.url}`)
